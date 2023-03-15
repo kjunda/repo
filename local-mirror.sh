432b2b4fb5b21914755cd678b0140d5330e3e46b
kjunda <54493316+kjunda@users.noreply.github.com>
2023-03-15 19:22:41 -0300

    Add files via upload

---
+++ local-mirror.sh
@@ -0,0 +1,2 @@
+cp /home/kjunda01/KJUNDA/local-mirror/sources.list /etc/apt/
+echo "Mirrors trocados para o 192.168.1.199/ubuntu - LOCAL"
---
+++ ubuntu-mirror.sh
@@ -0,0 +1,2 @@
+cp /home/kjunda01/KJUNDA/ubuntu-mirror/sources.list /etc/apt/
+echo "Mirrors trocados para o archive.ubuntu.com/ubuntu"
